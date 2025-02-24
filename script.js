a36d5afce4f16262331be4e57b5740060847a8f4
AnasSabbag <86312422+AnasSabbag@users.noreply.github.com>
2025-02-24 16:28:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let currElement=[];
+function handleOnClick(){
+
+	let elements=document.getElementById("colorSelect");
+
+	if(elements.length<=0)return;
+	elements.remove(0);
+	currElement=elements;
+	
+	document.getElementById("colorSelect")=currElement;
+
+}
